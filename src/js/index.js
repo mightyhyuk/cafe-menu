5af8899115a2e1f97af4d1212f544c8d47b0f5dd
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-28 22:08:04 +0900

    Add user value to menu list by handling DOM

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,36 @@
-function App() {
-  document
-    .querySelector("#espresso-menu-form")
-    .addEventListener("submit", (e) => e.preventDefault());
+const $ = (selector) => document.querySelector(selector);
 
-  document
-    .querySelector("#espresso-menu-name")
-    .addEventListener("keypress", (e) => {
-      if (e.key === "Enter")
-        console.log(document.querySelector("#espresso-menu-name").value);
-    });
+function App() {
+  $("#espresso-menu-form").addEventListener("submit", (e) =>
+    e.preventDefault()
+  );
+
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const espressoMenuName = $("#espresso-menu-name").value;
+      const menuItemTemplate = (espressoMenuName) => {
+        return `<li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>`;
+      };
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "beforeend",
+        menuItemTemplate(espressoMenuName)
+      );
+    }
+  });
 }
 
 App();

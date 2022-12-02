e88be21c20aa98a873ac2755a32586ee5069fa4b
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-12-02 22:15:00 +0900

    Add menu item when clicked

--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,16 @@ function App() {
     e.preventDefault()
   );
 
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
-    if (e.key !== "Enter") return;
-
+  const addMenuName = () => {
     if ($("#espresso-menu-name").value === "") {
       alert("값을 입력하세요.");
       return;
     }
 
-    if (e.key === "Enter") {
-      const espressoMenuName = $("#espresso-menu-name").value;
-      const menuItemTemplate = (espressoMenuName) => {
-        return `<li class="menu-list-item d-flex items-center py-2">
+    const espressoMenuName = $("#espresso-menu-name").value;
+
+    const menuItemTemplate = (espressoMenuName) => {
+      return `<li class="menu-list-item d-flex items-center py-2">
         <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
         <button
           type="button"
@@ -31,15 +29,28 @@ function App() {
           삭제
         </button>
       </li>`;
-      };
-      $("#espresso-menu-list").insertAdjacentHTML(
-        "beforeend",
-        menuItemTemplate(espressoMenuName)
-      );
-      const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-      $(".menu-count").innerText = `총 ${menuCount}개`;
-      $("#espresso-menu-name").value = "";
-    }
+    };
+
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      menuItemTemplate(espressoMenuName)
+    );
+
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+
+    $("#espresso-menu-name").value = "";
+  };
+
+  $("#espresso-menu-submit-button").addEventListener("click", () => {
+    addMenuName();
+  });
+
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key !== "Enter") return;
+
+    addMenuName();
   });
 }
 

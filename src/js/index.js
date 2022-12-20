8b153fbf5284bec036380a5a9843caebde5f423d
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-12-20 18:59:48 +0900

    Update addMenuName as async

--- src/js/index.js
+++ src/js/index.js
@@ -58,7 +58,7 @@ function App() {
     $(".menu-count").innerText = `총 ${menuCount}개`;
   };
 
-  const addMenuName = () => {
+  const addMenuName = async () => {
     if ($("#menu-name").value === "") {
       alert("값을 입력하세요.");
       return;
@@ -66,7 +66,7 @@ function App() {
 
     const menuName = $("#menu-name").value;
 
-    fetch(`${BASE_URL}/category/${this.currentCategory}/menu`, {
+    await fetch(`${BASE_URL}/category/${this.currentCategory}/menu`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -79,6 +79,15 @@ function App() {
       .then((data) => {
         console.log(data);
       });
+
+    await fetch(`${BASE_URL}/category/${this.currentCategory}/menu`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+
     // this.menu[this.currentCategory].push({ name: menuName });
     // store.setLocalStorage(this.menu);
 
